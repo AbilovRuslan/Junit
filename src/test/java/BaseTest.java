aa3c1598708828a05b0c1d309cb65718dea291a7
Abilov RU <ru_a@mail.ru>
2023-06-23 12:07:16 +0300

    add hw Junit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,20 @@
+package test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        //Configuration.headless = true;
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
+    }
+}
+
---
+++ src/test/java/WebTest.java
@@ -0,0 +1,46 @@
+package test;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WebTest extends test.BaseTest {
+
+
+    @DisplayName("Проверка поиска статьи на wikipedia")
+    @ValueSource(strings = {"Музыка", "Танец"})
+    @ParameterizedTest(name = "Проверка результата поиска для запроса {0}")
+    void wikipediaSearchTest(String testData) {
+        open("https://ru.wikipedia.org/");
+        $("#searchInput").setValue(testData);
+        $("#searchButton").click();
+        $(".mw-page-title-main").shouldHave(text(testData));
+    }
+
+    static Stream<Arguments> getTopicCategories() {
+        return Stream.of(
+                Arguments.of("Мужское", List.of("Выбор Oskelly", "Новинки", "Бренды", "Аксессуары", "Бьюти", "Обувь", "Одежда", "Сумки", "Украшения","Ювелирные изделия")),
+                Arguments.of("Женское", List.of("Выбор Oskelly", "Новинки", "Бренды", "Аксессуары", "Бьюти", "Обувь", "Одежда", "Сумки", "Украшения","Ювелирные изделия"))
+        );
+    }
+    @ParameterizedTest(name = "Соответствие списка категорий заданному топику {0}")
+    @MethodSource("getTopicCategories")
+    public void categoryShouldBeOpenAfterClick(String categoryName, List<String> buttonName) {
+        open("https://www.oskelly.ru/");
+        $(".osk-header").$(byText(categoryName)).click();
+        $$(".osk-header-bottom top").filter(visible).shouldHave(CollectionCondition.texts(buttonName));
+        }
+
+    }
+
