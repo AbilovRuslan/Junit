08d145bfa894469fdc9940325ec969f99807338d
Abilov RU <ru_a@mail.ru>
2023-07-13 21:49:38 +0300

    add hw Junit after CR

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,11 +1,10 @@
 package test;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -42,5 +41,18 @@ public class WebTest extends test.BaseTest {
         $$(".osk-header-bottom__link").filter(visible).shouldHave(CollectionCondition.texts(buttonName));
         }
 
+    @CsvSource({
+            "Танец, Та́нец — ритмичные, выразительные движения тела, обычно выстраиваемые в определённую композицию и исполняемые с музыкальным сопровождением",
+            "Музыка, Му́зыка (от др.-греч. μουσική[К 1]) — вид искусства, в котором определённым образом организованные звуки используются для создания некоторого сочетания формы, гармонии, мелодии, ритма или иного выразительного содержания."
+    })
+    @ParameterizedTest(name = "При вводе в поиск {0} на старнице присутствует текст {1}")
+    @DisplayName("Проверка поиска статьи на wikipedia")
+
+    void successfulSearchTextTest(String testData) {
+        open("https://ru.wikipedia.org/");
+        $("#searchInput").setValue(testData);
+        $("#searchButton").click();
+        $(".mw-page-title-main").shouldHave(text(testData));
     }
+}
 
